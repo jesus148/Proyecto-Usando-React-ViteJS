41dcfde4e92878e691c285c2436ebeaea16b96ac
jesus148 <jorgecop149@gmail.com>
2023-07-22 16:45:13 -0500

    other commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : 'https://github.com/jesus148/Proyecto-Usando-React-ViteJS.git',
+  base : '/Proyecto-Usando-React-ViteJS/',
 })
